4d00ef1a056b39bb949ce61b6028f40f6c68af79
Carlos Galo <cgalo13@gmail.com>
2021-09-04 16:31:26 -0400

    Added state inside app to remember logged user

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Switch} from 'react-router-dom';
 
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
 import Header from './components/Header/Header';
 import AuthPage from './pages/AuthPage/AuthPage';
-  
+
+import { auth } from './firebase/firebase-util';
+
+import type { User } from '@firebase/auth';
+
 import './App.css';
 
+
 const App: React.FC<{}> = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      setUser(user);
+    });
+    return () => setUser(null);
+  }, []);
   return (
     <div className="App">
       <Header />
